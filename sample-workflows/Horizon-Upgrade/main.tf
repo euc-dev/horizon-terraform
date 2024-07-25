66afa295f094b6e9603403dc8a59755cbb8ce997
guru1203-dev <bsg@omnissa.com>
2024-07-25 10:18:21 +0530

    Update main.tf

--- sample-workflows/Horizon-Upgrade/main.tf
+++ sample-workflows/Horizon-Upgrade/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     horizonview = {
-      source = "terraform-horizonview.com/horizonviewprovider/horizonview"
-      version= "1.1.123"
+      source = "custom/horizonviewprovider/horizonview"
+      version= "0.1.0"
     }
     null = {
       source  = "hashicorp/null"
